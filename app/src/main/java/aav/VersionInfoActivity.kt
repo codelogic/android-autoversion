39bfc3ba94b0609c155a7b71cb9b0704a19863b3
Paul Rohde <paul@paulrohde.com>
2018-12-27 21:16:52 -0800

    Fix version parsing for annotated tags.

--- app/src/main/java/aav/VersionInfoActivity.kt
+++ app/src/main/java/aav/VersionInfoActivity.kt
@@ -5,18 +5,16 @@ import android.os.Bundle
 import android.widget.TextView
 
 class VersionInfoActivity : Activity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_version_info)
-
-        val view = findViewById<TextView>(R.id.versionInfoText)
-        val versionInfo = "APPLICATION_ID: ${BuildConfig.APPLICATION_ID}\n" +
+    private val versionInfo = "APPLICATION_ID: ${BuildConfig.APPLICATION_ID}\n" +
             "BUILD_TYPE: ${BuildConfig.BUILD_TYPE}\n" +
             "DEBUG: ${BuildConfig.DEBUG}\n" +
             "FLAVOR: ${BuildConfig.FLAVOR}\n" +
             "VERSION_CODE: ${BuildConfig.VERSION_CODE}\n" +
             "VERSION_NAME: ${BuildConfig.VERSION_NAME}\n"
-        view.text = versionInfo
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_version_info)
+        findViewById<TextView>(R.id.versionInfoText).text = versionInfo
     }
 }
